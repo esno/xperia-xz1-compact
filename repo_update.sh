e9f3d0f3abfecb2c314d1f39571f521c019cfd65
crito <crito@fnordpipe.org>
2020-12-30 11:50:44 +0000

    aosp: remove default apps from build

--- repo_update.sh
+++ repo_update.sh
@@ -3,3 +3,8 @@
 # replace old precompiled lilac kernel
 cp ${ANDROID_BUILD_TOP}/vendor/xperia-xz1-compact/kernel/v4.14.208/kernel-dtb-lilac \
   ${ANDROID_BUILD_TOP}/kernel/sony/msm-4.14/common-kernel/kernel-dtb-lilac
+
+# remove default apps
+cd ${ANDROID_BUILD_TOP}/build
+patch -N -p 1 < ${ANDROID_BUILD_TOP}/vendor/xperia-xz1-compact/patches/remove-apps.patch
+cd ${ANDROID_BUILD_TOP}
