a2acd790ff59ca8249782a0b34857be894808a91
crito <crito@fnordpipe.org>
2020-12-30 21:45:55 +0000

    aosp9: add support for aosp 9

--- repo_update.sh
+++ repo_update.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-# replace old precompiled lilac kernel
-cp ${ANDROID_BUILD_TOP}/vendor/xperia-xz1-compact/kernel/v4.14.208/kernel-dtb-lilac \
-  ${ANDROID_BUILD_TOP}/kernel/sony/msm-4.14/common-kernel/kernel-dtb-lilac
+source ./build/envsetup.sh
+PLATFORM_VERSION=$(printconfig | grep -E "^PLATFORM_VERSION=" | awk -F '=' '{ print $2 }')
 
-# remove default apps
-cd ${ANDROID_BUILD_TOP}/build
-patch -N -p 1 < ${ANDROID_BUILD_TOP}/vendor/xperia-xz1-compact/patches/remove-apps.patch
-cd ${ANDROID_BUILD_TOP}
+update_aosp9() {
+  # remove default apps
+  cd ./build
+  patch -N -p 1 < ../vendor/xperia-xz1-compact/aosp9/patches/remove-apps.patch
+  cd ..
+}
+
+update_aosp11() {
+  # replace old precompiled lilac kernel
+  cp ${ANDROID_BUILD_TOP}/vendor/xperia-xz1-compact/kernel/v4.14.208/kernel-dtb-lilac \
+    ${ANDROID_BUILD_TOP}/kernel/sony/msm-4.14/common-kernel/kernel-dtb-lilac
+
+  # remove default apps
+  cd ${ANDROID_BUILD_TOP}/build
+  patch -N -p 1 < ${ANDROID_BUILD_TOP}/vendor/xperia-xz1-compact/aosp11/patches/remove-apps.patch
+  cd ${ANDROID_BUILD_TOP}
+}
+
+case "${PLATFORM_VERSION}" in
+  9)
+    update_aosp9
+    ;;
+  11)
+    update_aosp11
+    ;;
+esac
